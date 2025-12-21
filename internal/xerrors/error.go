c6760a5da2bfa2c103d9e93cc8d30581d092ac7e
Garrett Ladley <92384606+garrettladley@users.noreply.github.com>
2025-12-20 21:28:01 -0500

    feat: object impl (#2)

---
+++ internal/xerrors/error.go
@@ -0,0 +1,88 @@
+package merkle
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/garrettladley/smerkle/internal/object"
+)
+
+type HashError struct {
+	Path string
+	Err  error
+}
+
+func (e HashError) Error() string {
+	return fmt.Sprintf("%s: %v", e.Path, e.Err)
+}
+
+func (e HashError) Unwrap() error {
+	return e.Err
+}
+
+type Result struct {
+	Hash   object.Hash
+	Errors []HashError
+}
+
+func (r *Result) Ok() bool {
+	return len(r.Errors) == 0
+}
+
+func (r *Result) Err() error {
+	if r.Ok() {
+		return nil
+	}
+	return &MultiError{Errors: r.Errors}
+}
+
+type MultiError struct {
+	Errors []HashError
+}
+
+func (e *MultiError) Error() string {
+	if len(e.Errors) == 1 {
+		return e.Errors[0].Error()
+	}
+
+	var b strings.Builder
+	fmt.Fprintf(&b, "%d errors occurred:\n", len(e.Errors))
+	for _, err := range e.Errors {
+		fmt.Fprintf(&b, "  - %s\n", err.Error())
+	}
+	return b.String()
+}
+
+type ErrorCollector struct {
+	errors []HashError
+	mu     chan struct{}
+}
+
+func NewErrorCollector() *ErrorCollector {
+	ec := &ErrorCollector{
+		mu: make(chan struct{}, 1),
+	}
+	ec.mu <- struct{}{}
+	return ec
+}
+
+func (c *ErrorCollector) Add(path string, err error) {
+	<-c.mu
+	c.errors = append(c.errors, HashError{Path: path, Err: err})
+	c.mu <- struct{}{}
+}
+
+func (c *ErrorCollector) Errors() []HashError {
+	<-c.mu
+	result := make([]HashError, len(c.errors))
+	copy(result, c.errors)
+	c.mu <- struct{}{}
+	return result
+}
+
+func (c *ErrorCollector) HasErrors() bool {
+	<-c.mu
+	has := len(c.errors) > 0
+	c.mu <- struct{}{}
+	return has
+}
